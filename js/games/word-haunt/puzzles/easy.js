de239a9f55e0a47e847f7b2937bfb5884577e431
tristanpendergrass <tristanpendergrass@gmail.com>
2025-10-16 15:20:43 -0700

    Design easy puzzle properly

--- js/games/word-haunt/puzzles/easy.js
+++ js/games/word-haunt/puzzles/easy.js
@@ -1,33 +1,33 @@
 /**
  * Easy Halloween Word Haunt Puzzle
- * 
+ *
  * A 6x6 grid with simple, short Halloween words
  * Perfect for beginners or quick games
  */
 
 module.exports = {
-    name: "Easy Halloween Haunt",
-    description: "Find simple spooky words in this beginner-friendly grid!",
-    
+    name: "Spotted on Halloween night...",
+    description: "Find all the words",
+
     // 6 rows x 6 columns grid
     grid: [
-        ['G', 'H', 'O', 'S', 'T', 'M'],
-        ['O', 'A', 'R', 'A', 'N', 'U'],
-        ['B', 'T', 'I', 'C', 'K', 'M'],
-        ['L', 'K', 'N', 'G', 'L', 'M'],
-        ['I', 'C', 'A', 'T', 'S', 'Y'],
-        ['N', 'E', 'B', 'A', 'T', 'S']
+        ['F', 'I', 'R', 'E', 'F', 'I', 'G', 'H',],
+        ['L', 'S', 'A', 'B', 'U', 'Z', 'Z', 'T',],
+        ['E', 'Y', 'A', 'D', 'S', 'U', 'M', 'E',],
+        ['W', 'E', 'D', 'N', 'E', 'R', 'I', 'R',],
+        ['N', 'A', 'M', 'R', 'E', 'P', 'U', 'S',],
     ],
-    
+
     // Target words to find - all simple, short Halloween words
     words: [
-        'GHOST',    // 5 letters - horizontal, top row
-        'HAT',      // 3 letters - vertical, column 2
-        'CATS',     // 4 letters - horizontal, row 5
-        'BATS',     // 4 letters - horizontal, bottom row
-        'TRICK'     // 5 letters - vertical, columns scattered
+        'BUZZ',
+        'ELSA',
+        'FIREFIGHTER',
+        'RUMI',
+        'SUPERMAN',
+        'WEDNESDAY',
     ],
-    
-    gridSize: 36,           // 6 * 6 = 36
-    totalWordLength: 21     // 5+3+4+4+5 = 21
+
+    gridSize: 40,
+    totalWordLength: 40
 };
